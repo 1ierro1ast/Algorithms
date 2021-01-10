f072013b2d6c05827da77443b9b6a4e169f1f514
1ierro1ast <lierro00@gmail.com>
2021-01-10 22:13:48 +0300

    dijkstras algorithm added

--- Search/DijkstrasAlgorithm/DijkstrasAlgorithm.py
+++ Search/DijkstrasAlgorithm/DijkstrasAlgorithm.py
@@ -1,1 +1,31 @@
 ##
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+print(graph)
+print(costs)
+print(parents)

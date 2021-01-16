aef157ebddeb85e0536c68f472a2454b92c651e6
1ierro1ast <lierro00@gmail.com>
2021-01-16 12:31:14 +0300

    dijkstras algorithm added

--- Search/DijkstrasAlgorithm/DijkstrasAlgorithm.py
+++ Search/DijkstrasAlgorithm/DijkstrasAlgorithm.py
@@ -1,4 +1,8 @@
 ##
+
+"""
+O(n^2)
+"""
 graph = {}
 graph["start"] = {}
 graph["start"]["a"] = 6
@@ -29,3 +33,39 @@ processed = []
 print(graph)
 print(costs)
 print(parents)
+
+def findLowestCostNode(costs: dict):
+	lowestCost = float("inf")
+	lowestCostNode = None
+	for node in costs:
+		cost = costs[node]
+		if cost < lowestCost and node not in processed:
+			lowestCost = cost
+			lowestCostNode = node
+	return lowestCostNode
+
+node = findLowestCostNode(costs)
+while node is not None:
+	cost = costs[node]
+	neighbors = graph[node]
+	for n in neighbors.keys():
+		newCost = cost + neighbors[n]
+		if costs[n] > newCost:
+			costs[n] = newCost
+			parents[n] = node
+	processed.append(node)
+	node = findLowestCostNode(costs)
+
+print(parents)
+
+def createPath(startNode, endNode, parentsDict):
+	curNode = startNode
+	path = [startNode]
+	while curNode != endNode:
+		for i in parentsDict:
+			if parentsDict[i] == curNode:
+				path.append(i)
+				curNode = i
+	return path
+
+print(createPath("start", "fin", parents))
